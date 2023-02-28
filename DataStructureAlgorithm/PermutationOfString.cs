346aeab49a2c5c17a1746cdee6a81fc5ce16fd12
vaibhav <vaibhavmehta524@gmail.com>
2023-02-28 23:42:04 +0530

    Modified to Permutation of String Program

---
+++ DataStructureAlgorithm/PermutationOfString.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureAlgorithm
+{
+    public class PermutationOfString
+    {
+        public static void Permute(String s, String answer)
+        {
+            if (s.Length == 0)
+            {
+                Console.Write(answer + "  ");
+                return;
+            }
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                char ch = s[i];
+                String left_substr = s.Substring(0, i);
+                String right_substr = s.Substring(i + 1);
+                String res = left_substr + right_substr;
+                Permute(res, answer + ch);
+            }
+        }
+    }
+}
---
+++ DataStructureAlgorithm/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DataStructureAlgorithm
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to data structure algorithum");
+            Console.WriteLine("Please enter an option");
+            Console.WriteLine("1:Permutation of string");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Hello, World!");
+                    String s = "ABC";
+                    String answer = "";
+                    Console.Write("\nAll possible strings are : ");
+                    PermutationOfString.Permute(s, answer);
+                    break;
+                default:
+                    Console.WriteLine("Please enter right option");
+                    break;
+            }
+        }
+    }
+}
