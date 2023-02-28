bd32c846dc93af31b668a287911494e1a6ace1a6
vaibhav <vaibhavmehta524@gmail.com>
2023-02-28 23:49:05 +0530

    Modified to Binary Search Program

---
+++ DataStructureAlgorithm/BinarySearch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureAlgorithm
+{
+    public class BinarySearch
+    {
+        public static void result(string[] arr2, object k)
+        {
+            int res = Array.BinarySearch(arr2, k);
+            if (res < 0)
+            {
+                Console.WriteLine("\nThe element to search for {0} is not found.", k);
+            }
+            else
+            {
+                Console.WriteLine("The element to search for {0} is at index {1}", k, res); ;
+            }
+        }
+        public static void display(string[] arr1)
+        {
+            foreach (string i in arr1)
+                Console.WriteLine(i + " ");
+        }
+    }
+}
--- DataStructureAlgorithm/Program.cs
+++ DataStructureAlgorithm/Program.cs
@@ -8,7 +8,7 @@ namespace DataStructureAlgorithm
         {
             Console.WriteLine("Welcome to data structure algorithum");
             Console.WriteLine("Please enter an option");
-            Console.WriteLine("1:Permutation of string");
+            Console.WriteLine("1:Permutation of string\n2:Binary search");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -18,6 +18,16 @@ namespace DataStructureAlgorithm
                     String answer = "";
                     Console.Write("\nAll possible strings are : ");
                     PermutationOfString.Permute(s, answer);
+                    break;
+                case 2:
+                    string[] arr = new string[] { "abc", "def", "ghi", "jkl" };
+                    Array.Sort(arr);
+                    Console.WriteLine("The elements of Sorted Array");
+                    BinarySearch.display(arr);
+                    object s1 = "mno";
+                    BinarySearch.result(arr, s1);
+                    object s2 = "def";
+                    BinarySearch.result(arr, s2);
                     break;
                 default:
                     Console.WriteLine("Please enter right option");
